3c9f4abb4de3d18c76aca86cbd54bcbcb98f57ec
Felipe Tavares <xfelipetavares@gmail.com>
2022-11-14 20:43:15 -0300

    por hoje deu

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,20 @@
-
-
-export default function Home() {
+export default function Home(props) {
   return (
-    <div>Index</div>
-  )
+    <div>
+      <div>Index a</div>
+      <>{console.log(props.episodes)}</>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  };
 }
